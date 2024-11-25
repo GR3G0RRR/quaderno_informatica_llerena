3ec882753e46a023343eac009ca05609a883b4df
gr3g0r <gabriele.llerena@gmail.com>
2024-11-25 14:46:15 +0100

    esercizio 1 rinominato

---
+++ esercizio 1/esercizio1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ita">
+    <head>
+        <meta charset="UTF-8">
+        <meta name = "viewport" content="width=device-width, initial-scale=1.0">
+        <title>esercizio1_1</title>
+    </head>
+
+<body>
+    <h1>esercizio 1:</h1>
+    <hr>
+    <h2>TABELLA PITTAGORICA</h2>
+<table border = 3>
+<?php
+$size = 10;
+echo "<tr><th>*</th>";
+for($i = 1; $i <= $size; $i++){
+    echo "<th>$i</th>";
+}
+echo "</tr>";
+for($i = 1; $i <= $size; $i++){
+    echo "<tr>";
+    echo "<th>$i</th>";
+    for($j = 1; $j <= $size; $j++){
+        echo "<td>". ($i * $j)."</td>";
+    }
+    echo "</tr>";
+}
+?>
+</table>
+
+</body>
+</html>
